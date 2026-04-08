38e479dab535be2e7b9849d5fa16d5eafe0f52c4
guihgalegodev <guigalego96@gmail.com>
2026-04-08 18:30:29 -0300

    first-commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const modal = document.getElementById("modal");
+const fechar = document.querySelector(".fechar");
+
+function abrirModal() {
+  modal.style.display = "block";
+}
+
+fechar.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+//https://shopee.com.br/Jogos-Seminovos-de-PS3-Originais-i.490386670.58206952976?extraParams=%7B%22display_model_id%22%3A159552964230%2C%22model_selection_logic%22%3A3%7D
+
+//https://shopee.com.br/Jogos-Seminovos-de-PS3-Originais-i.490386670.58206952976?extraParams=%7B%22display_model_id%22%3A159552964230%2C%22model_selection_logic%22%3A3%7D
