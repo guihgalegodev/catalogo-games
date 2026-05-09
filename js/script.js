be625534a479b14d25de1061107b87d1b17fd3c5
guihgalegodev <guigalego96@gmail.com>
2026-05-09 15:40:32 -0300

    efeito de black-box add

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ import { games } from "./modules/games.js";
 import { abrirModal, initModalEvents } from "./modules/modal.js";
 
 const btnDetalhes = document.querySelectorAll(".details");
+const isMobile = window.innerWidth <= 980;
+
+if (isMobile) {
+  const cards = document.querySelectorAll(".jogo-img");
+
+  cards.forEach((card) => {
+    setInterval(() => {
+      card.classList.toggle("ativo");
+    }, 3000);
+  });
+}
 
 btnDetalhes.forEach((btn) => {
   btn.addEventListener("click", () => {
