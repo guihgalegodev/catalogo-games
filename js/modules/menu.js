72a66fa9caf8c07a1f00a79d8e15534873b14c89
guihgalegodev <guigalego96@gmail.com>
2026-05-14 13:35:14 -0300

    ajustes-menu-mobile

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,10 +1,12 @@
 export function initMenuMobile() {
   const btnMobile = document.getElementById("btn-mobile");
 
-  function abrirFecharMenu() {
+  function abrirFecharMenu(e) {
+    if (e.type === "touchstart") e.preventDefault();
     const nav = document.getElementById("nav-menu");
     nav.classList.toggle("active");
   }
 
+  btnMobile.addEventListener("touchstart", abrirFecharMenu);
   btnMobile.addEventListener("click", abrirFecharMenu);
 }
